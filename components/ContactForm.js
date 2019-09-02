9a7ead4a4052678a296701b233aea36a2f85582c
Michał Nowakowski <nowakowskimichal1988@gmail.com>
2019-09-02 20:18:39 +0200

    18.7_v7

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -28,7 +28,7 @@ var ContactForm = React.createClass({
           placeholder='Email'
           id='inputYourEmail'
           required
-          value{this.props.contact.email}
+          value={this.props.contact.email}
         />
         <button type='submit'>Dodaj kontakt</button>
       </form>
